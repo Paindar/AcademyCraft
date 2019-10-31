1d3e946048412ea6b9b7211d6e4b64a1324fefc0
Paindar <16387007+Paindar@users.noreply.github.com>
2019-11-01 02:20:23 +0800

    Add GetAcCfg anno.

---
+++ src/main/java/cn/academy/event/ConfigUpdateEvent.java
@@ -0,0 +1,9 @@
+package cn.academy.event;
+
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+public class ConfigUpdateEvent  extends Event {
+    public ConfigUpdateEvent() {
+
+    }
+}
